6b5b92f8c0fb63ded287345930b4f65450eebc79
Leo Luiten <leonardoluiten@gmail.com>
2024-08-12 10:42:08 +0200

    Added some spring doc annotations for the Player model and the player controller

--- src/main/java/leoluiten/presentation/controllers/PlayerController.java
+++ src/main/java/leoluiten/presentation/controllers/PlayerController.java
@@ -1,6 +1,12 @@
 package leoluiten.presentation.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import leoluiten.presentation.dtos.common.ErrorApi;
 import leoluiten.presentation.models.Player;
 import leoluiten.presentation.services.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +28,19 @@ public class PlayerController {
     @Autowired
     private PlayerService playerService;
 
+    @Operation(
+            summary = "Gets a player by its id.",
+            description = "Returns either a matching Player or a 404 status code error."
+    )
+    @ApiResponses( value = {
+            @ApiResponse( responseCode = "200", description = "Successful operation", content =
+                @Content(schema = @Schema(implementation = Player.class))),
+            @ApiResponse( responseCode = "404", description = "Not found", content =
+                @Content(schema = @Schema(implementation = ErrorApi.class))),
+            @ApiResponse( responseCode = "500", description = "Internal server error", content =
+                @Content(schema = @Schema(implementation = ErrorApi.class)))
+    })
+
     /**
      * Retrieves a player by their unique ID.
      *
@@ -32,6 +51,20 @@ public class PlayerController {
     public ResponseEntity<Player> getById(@PathVariable Long id) {
         return ResponseEntity.ok(playerService.getPlayerById(id));
     }
+
+    @Operation(
+            summary = "Create a new player",
+            description = "Return the player created with your id. If a player matching the username or email exists, then it will return 404. " +
+                    "Additionally, the email must be valid and the password must have at least 8 characters while containing at least one number " +
+                    " one lowercase letter, one uppercase letter and one special character")
+            @ApiResponses( value = {
+                    @ApiResponse(responseCode = "200", description = "Successful operation", content =
+                        @Content(schema = @Schema(implementation = Player.class))),
+                    @ApiResponse(responseCode = "400", description = "Username or email already exists", content =
+                        @Content(schema = @Schema(implementation = ErrorApi.class))),
+                    @ApiResponse(responseCode = "500", description = "Internal server error", content =
+                        @Content(schema = @Schema(implementation = ErrorApi.class)))
+            })
 
     /**
      * Saves a player's data.
--- src/main/java/leoluiten/presentation/models/Player.java
+++ src/main/java/leoluiten/presentation/models/Player.java
@@ -1,8 +1,10 @@
 package leoluiten.presentation.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import leoluiten.presentation.utils.validations.password.ValidPassword;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,18 +16,27 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Player {
 
+    @Schema(title = "Player'S ID", description = "An unique value for identifying the player", example = "1")
     private Long id;
 
+    @Schema(title = "Player's user name", description = "The player's name that will be displayed to other users", example = "FeelzGood", nullable = false)
     @NotNull(message = "UserName can't be null")
     private String userName;
+
+    @Schema(title = "Player's password", description = "The password for login in", example = "Password#03")
     @NotNull(message = "Password can't be null")
+    @ValidPassword
     private String password;
 
+    @Schema(title = "Player's email address", description = "The email account for recovering the password and receiving notifications", example = "email@email.com")
     @NotNull(message = "Email cant be null")
     @Email(message = "The email should be written with a conventional format")
     private String email;
+
+    @Schema(title = "Player's avatar url", description = "The url for the customizable avatar to show in game", example = "https://localhost:8080/avatars/myUsername", nullable = true)
     private String avatar;
 
+    @Schema(title = "Player's last login date", description = "States the last registered player's login date", example = "15-12-2029 13:24:40", nullable = true)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
     private LocalDateTime lastLogin;
 }
